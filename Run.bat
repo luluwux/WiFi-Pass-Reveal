6d9dc024e29cea978cc0043b9d7e8b5f99b7ecbb
Alp 'Lulu <zubeyr0.0alp@gmail.com>
2026-05-15 09:49:12 +0300

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -2,8 +2,14 @@
 title WifiPass Viewer
 
 net session >nul 2>&1
+if %errorLevel% == 0 (
+    goto :run
+) else (
+    echo Requesting administrative privileges...
+    powershell -Command "Start-Process -FilePath '%0' -Verb RunAs"
+    exit /b
+)
 
 :run
 cd /d "%~dp0"
-
 powershell -ExecutionPolicy Bypass -NoProfile -File "WifiPassReveal.ps1"
